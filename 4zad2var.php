7e7fc3039e7ffd3cba29ac74caf33e290b4d725e
alex_f9t <alexandra.1992@abv.bg>
2014-11-23 20:42:51 +0200

    Homework-php

---
+++ 4zad2var.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Задача 4/2</title>
+<body>
+   <?php 
+	$int_var = "2";
+	
+	switch ($int_var) {
+		case 1:
+	    echo "<div class='first'>Аз съм произволен текст!</div>";
+		break;
+		
+		case 2:
+		echo "<div class='second'></div>";
+		break;
+		
+		case 3:
+		echo "<span class='third'>Аз съм произволен текст!</span>";
+		break;
+	}
+	?>
+
+</body>
+<style type="text/css">
+.first {background:blue;}
+.second {width:600px; height:372px; background-image: url(kola.jpg); border:1px solid red;}
+.third {background:#000; color:#fff;}
+</style>
+</head>
+</html>
+
---
+++ 5zad.php
@@ -0,0 +1,26 @@
+﻿
+<html>
+<body>
+<style>
+	body {
+		font-size:26px;
+	}
+</style>
+<?php
+
+        $int_var = 49;
+
+      if ($int_var >= -100 && $int_var < 0) {
+    echo "Отрицателно число";
+	}  else if ($int_var >= 0 && $int_var < 50) {
+    echo "Число между 0 и 50";
+	 } 
+	 else { 
+	    echo "Числото е по-голямо от 50";
+	  }
+	 
+	
+	  
+?>
+</body>
+</html>
