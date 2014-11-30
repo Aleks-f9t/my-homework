0a048fca5c4f61eeb9c6fd30dbdd5caefc8ebf22
alex_f9t <alexandra.1992@abv.bg>
2014-11-30 21:57:47 +0200

    php part 2

---
+++ for_2.php
@@ -0,0 +1,39 @@
+﻿﻿<!DOCTYPE>
+<html>
+<head>
+<title>Table_for</title>
+</head>
+<body>
+
+<table border="5">
+
+<?php
+$number = 0;
+	for ($a = 1; $a < 5; $a++) {
+			echo "<tr>"; 
+	for ($b = 1; $b < 6; $b++) {
+			
+			$number++;
+			
+			if ($number % 2 == 0) {
+				$result = "<td style='background:yellow;'>" . $number . "</td>";
+			} else {
+				$result = "<td style='background:green;'>" . $number . "</td>";
+				}
+				
+			if ($number % 2 == 0 && $number > 12) {
+				$result = "<td style='color:red;'>" . $number . "</td>";
+				}
+			else if ($number % 2 !== 0 && $number > 12) {
+				$result = "<td style='color:red;'>" . $number . "</td>";
+				}
+			
+			echo $result;	
+		}
+	echo "</tr>";
+}
+?>
+
+</table>
+</body>
+</html>
---
+++ while_2.php
@@ -0,0 +1,43 @@
+﻿﻿<!DOCTYPE>
+<html>
+<head>
+
+<title>Table_while</title>
+</head>
+<body>
+
+<table border="5">
+
+<?php
+$number = 0;
+$a = 1;
+while ($a < 5) {
+		$a++;
+		echo "<tr>"; 
+		$b = 1;
+while ($b < 6) {
+	    $b++;
+	    $number++;
+			
+			if ($number % 2 == 0) {
+				$result = "<td style='background:yellow;'>" . $number . "</td>";
+			} else {
+				$result = "<td style='background:green;'>" . $number . "</td>";
+				}
+				
+			if ($number % 2 == 0 && $number > 12) {
+				$result = "<td style='color:red;'>" . $number . "</td>";
+				}
+			else if ($number % 2 !== 0 && $number > 12) {
+				$result = "<td style='color:red;'>" . $number . "</td>";
+				}
+			
+			echo $result;	
+		}
+		echo "</tr>";
+}
+?>
+
+</table>
+</body>
+</html>
