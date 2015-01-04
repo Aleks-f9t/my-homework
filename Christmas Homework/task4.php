d933a36174b18fb27ecce0f5e2f0d51a6b62c71b
alex_f9t <alexandra.1992@abv.bg>
2015-01-04 14:34:57 +0200

    Christmas homework

---
+++ Christmas Homework/task4.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+  <title></title>
+</head>
+   <style>
+	body {
+		font-size:26px;
+	}
+	form{
+		font-size: 25px;
+		padding: 30px;
+		margin:30px;
+	}
+	form > div {
+		margin-bottom: 30px;
+	}
+</style>
+
+<body>
+
+
+<form name="" action="" method="post">
+<textarea name="Name" rows=5 cols=50 wrap="off"></textarea>
+ <div>
+<input type="submit" value="SEND">
+     </div>
+
+</form>
+
+</body>
+
+</html>
